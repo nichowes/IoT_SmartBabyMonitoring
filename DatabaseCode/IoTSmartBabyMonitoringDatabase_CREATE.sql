2bbf5092a31c829bfb4d9e760938e805aaf01fc3
samykibrahim <51800988+samykibrahim@users.noreply.github.com>
2019-10-26 15:50:02 -0400

    Code to create the database using the schema.

---
+++ DatabaseCode/IoTSmartBabyMonitoringDatabase_CREATE.sql
@@ -0,0 +1,60 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2019-10-26 19:44:32.445
+
+-- tables
+-- Table: BABY_ANALYSIS
+CREATE TABLE BABY_ANALYSIS (
+    id int NOT NULL,
+    babyName varchar(255) NOT NULL,
+    babyAge int NOT NULL,
+    babyHeight int NOT NULL,
+    babyWeight int NOT NULL,
+    averageNumberOfWakeupsPerDay int NOT NULL,
+    averageWakeupTimesPerDay varchar(255) NOT NULL,
+    averageNumberOfFeedsPerDay int NOT NULL,
+    averageFeedTimesPerDay varchar(255) NOT NULL,
+    favouriteDayTimeCalmDownMethod int NOT NULL,
+    favouriteNightTimeCalmDownMethod int NOT NULL,
+    preferredTemperature int NOT NULL,
+    CONSTRAINT BABY_ANALYSIS_pk PRIMARY KEY (id)
+) COMMENT 'All information relating to the baby analysis.';
+
+-- Table: BABY_CALM_METHODS
+CREATE TABLE BABY_CALM_METHODS (
+    id int NOT NULL,
+    name varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    priority int NOT NULL,
+    CONSTRAINT BABY_CALM_METHODS_pk PRIMARY KEY (id)
+) COMMENT 'All the methods the system uses to ';
+
+-- Table: GENERAL_DATA_AND_SETTINGS
+CREATE TABLE GENERAL_DATA_AND_SETTINGS (
+    id int NOT NULL,
+    userName varchar(255) NOT NULL,
+    babyName varchar(255) NOT NULL,
+    waitTimeAfterTryingCalmMethods int NOT NULL,
+    CONSTRAINT GENERAL_DATA_AND_SETTINGS_pk PRIMARY KEY (id)
+) COMMENT 'All general information relating to the system and its users.';
+
+-- Table: SENSOR_DATA
+CREATE TABLE SENSOR_DATA (
+    id int NOT NULL,
+    timestamp timestamp NOT NULL,
+    temperature_sensor_Temperature int NOT NULL,
+    temperature_sensor_Humidity int NOT NULL,
+    CONSTRAINT SENSOR_DATA_pk PRIMARY KEY (id)
+);
+
+-- foreign keys
+-- Reference: BABY_ANALYSIS_BABY_CALM_METHODS (table: BABY_ANALYSIS)
+ALTER TABLE BABY_ANALYSIS ADD CONSTRAINT BABY_ANALYSIS_BABY_CALM_METHODS FOREIGN KEY BABY_ANALYSIS_BABY_CALM_METHODS (favouriteDayTimeCalmDownMethod)
+    REFERENCES BABY_CALM_METHODS (id);
+
+-- Reference: favouriteNightTimeCalmDownMethod (table: BABY_ANALYSIS)
+ALTER TABLE BABY_ANALYSIS ADD CONSTRAINT favouriteNightTimeCalmDownMethod FOREIGN KEY favouriteNightTimeCalmDownMethod (favouriteNightTimeCalmDownMethod)
+    REFERENCES BABY_CALM_METHODS (id);
+
+-- End of file.
+
